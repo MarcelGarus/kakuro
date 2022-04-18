13c528c8510694f1c7fe6adf67beb1e719f58f50
Marcel Garus <marcel.garus@gmail.com>
2022-04-18 14:00:30 +0200

    Update solver docs

--- src/solvers/mod.rs
+++ src/solvers/mod.rs
@@ -1,25 +1,25 @@
 //! This module contains different solvers to solve Kakuros:
 //!
-//! - naive: This solver considers all combinations of filling out cells. For
+//! - naive: This solver constructs all combinations of filling out cells. For
 //!   each combination, it checks if it's a valid solution.
-//! - gradual: This solver can evaluate partially filled out Kakuros. It fills
-//!   cells with numbers one at a time and tries to find a valid combination by
-//!   filling the cells one at a time and early-aborting if the Kakuro becomes
-//!   invalid.
-//! - early_abort: Like gradual, but also aborts if a sum cannot possibly be
-//!   reached any longer. For example, if there's a constraint with the sum for
-//!   the cells 4 _ 9, then the partially filled out game is already considered
-//!   invalid.
-//! - prioritize: Like early_abort, but doesn't just fill the cells in the
+//! - gradual: This solver can evaluate partially filled out Kakuros. It tries
+//!   to find a valid combination by filling the cells one at a time and
+//!   aborting if the Kakuro becomes invalid by having the same digit in a
+//!   constraint twice or a filled constraint not having the right sum.
+//! - sum_reachable: Like gradual, but also aborts if a constraints sum cannot
+//!   possibly be reached any longer by filling out unused numbers.
+//! - prioritize: Like sum_reachable, but doesn't just fill the cells in the
 //!   arbitrary order that they were numbered in. Instead, it fills the first
 //!   cell and then cells in rows and columns that already contain numbers.
-//! - divide: On a large board, it doesn't make sense to even attempt to look at
-//!   all combinations. Instead, this solver divides a big Kakuro into two
-//!   smaller ones that are only connected with few constraints. For those
-//!   smaller games, all constraints still apply, except those spanning both.
-//!   Both smaller Kakuros are solved in isolation and the product of both
-//!   result sets is filtered to the solutions also fulfilling the connecting
-//!   constraints.
+//! - prioritize_no_set: Like prioritize, but without using HashSets to check
+//!   for uniqueness.
+//! - sum_reachable_no_set: Like sum_reachable, but without using HashSets to
+//!   check for uniqueness.
+//! - divide: This solver divides a big Kakuro into two smaller ones that are
+//!   only connected with few constraints. All constraints except those
+//!   connecting both parts still apply. Both smaller Kakuros are solved in
+//!   isolation and the product of both result sets is filtered to the solutions
+//!   also fulfilling the connecting constraints.
 //! - lazy: Like divide, but the recursive solving communicates which parts of
 //!   the board play a role in connecting constraints later on. This allows
 //!   inner calls to not actually construct solutions that are equivalent in
@@ -43,3 +43,4 @@ pub mod naive;
 pub mod prioritize;
 pub mod prioritize_no_set;
 pub mod sum_reachable;
+pub mod sum_reachable_no_set;
