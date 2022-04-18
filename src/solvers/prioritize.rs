c4c7257ed616955f8156bd649e3d7d6d37abb5f3
Marcel Garus <marcel.garus@gmail.com>
2022-04-18 13:00:22 +0200

    Clean up the prioritize solver

--- src/solvers/prioritize.rs
+++ src/solvers/prioritize.rs
@@ -1,8 +1,9 @@
-use std::collections::HashSet;
-
+use crate::{
+    game::{Input, Output, Solution, Value},
+    log::log,
+};
 use itertools::Itertools;
-
-use crate::game::{Input, Output, Solution, Value};
+use std::collections::HashSet;
 
 type Game = Vec<Cell>;
 type Cell = Option<Value>;
@@ -18,16 +19,17 @@ impl InputExt for Input {
                 .iter()
                 .map(|b| attempt[*b])
                 .collect::<Vec<_>>();
-            let numbers = cells.into_iter().filter_map(|it| it).collect::<Vec<_>>();
+            let numbers = cells.into_iter().filter_map(|it| it).collect_vec();
+            let len = numbers.len();
 
-            if numbers.iter().collect::<HashSet<_>>().len() < numbers.len() {
+            let numbers = numbers.into_iter().collect::<HashSet<_>>();
+            if numbers.len() < len {
                 return false; // A number appears twice.
             }
 
-            let numbers = numbers.into_iter().collect::<HashSet<_>>();
             let sum: Value = numbers.iter().sum();
             if sum == 0 {
-                continue; // No cells filled in yet; we assume the game itself is possible.
+                continue; // No cells filled in yet; we assume the attempt is possible.
             }
 
             let possible_digits = (1..=9u8)
@@ -56,54 +58,65 @@ pub fn solve(input: &Input) -> Output {
 }
 
 fn solve_rec(input: &Input, attempt: &mut Game, solutions: &mut Vec<Solution>) {
-    // println!(
-    //     "Evaluating attempt {}",
-    //     attempt
-    //         .iter()
-    //         .map(|cell| {
-    //             match cell {
-    //                 Some(number) => format!("{}", number),
-    //                 None => "-".to_string(),
-    //             }
-    //         })
-    //         .collect::<String>()
-    // );
+    log(format!(
+        "Evaluating attempt {}",
+        attempt
+            .iter()
+            .map(|cell| {
+                match cell {
+                    Some(number) => format!("{}", number),
+                    None => "-".to_string(),
+                }
+            })
+            .join("")
+    ));
+
     if !input.is_possible_solution(attempt) {
         return;
     }
-    // For each index, how many partially filled constraints contain it.
+
+    // For each cell, save how many partially filled constraints contain it.
     let mut index_priorities = vec![0; input.num_cells];
-    input
-        .constraints
-        .iter()
-        .map(|it| &it.cells)
-        .filter(|indizes| indizes.iter().any(|index| attempt[*index].is_some()))
-        .for_each(|constraint| {
-            for i in constraint {
+    for constraint in &input.constraints {
+        let is_partially_filled = constraint
+            .cells
+            .iter()
+            .any(|index| attempt[*index].is_some());
+        if is_partially_filled {
+            for i in &constraint.cells {
                 if let None = attempt[*i] {
                     index_priorities[*i] += 1;
                 }
             }
-        });
-    let index_to_fill = if index_priorities.iter().any(|it| *it > 0) {
-        index_priorities
-            .into_iter()
-            .enumerate()
-            .max_by_key(|it| it.1)
-            .map(|it| it.0)
-    } else {
-        // No constraint contains a number _and_ an empty cell.
-        attempt.iter().position(|it| it.is_none())
+        }
+    }
+
+    let highest_priority_cell = index_priorities
+        .into_iter()
+        .enumerate()
+        .max_by_key(|it| it.1);
+    let (cell, priority) = match highest_priority_cell {
+        Some(it) => it,
+        None => {
+            // This is a solution.
+            solutions.push(attempt.iter().map(|it| it.unwrap()).collect());
+            return;
+        }
     };
 
-    if let Some(index) = index_to_fill {
-        for i in 1..=9 {
-            attempt[index] = Some(i);
-            solve_rec(input, attempt, solutions);
-        }
-        attempt[index] = None;
+    let cell_to_fill = if priority > 0 {
+        // The cell is guaranteed to be empty because only the priority of empty
+        // cells was increased before.
+        cell
     } else {
-        // This is a solution.
-        solutions.push(attempt.iter().map(|it| it.unwrap()).collect());
+        // No constraint contains a number _and_ an empty cell. Just fill the
+        // first empty cell.
+        attempt.iter().position(|it| it.is_none()).unwrap()
+    };
+
+    for i in 1..=9 {
+        attempt[cell_to_fill] = Some(i);
+        solve_rec(input, attempt, solutions);
     }
+    attempt[cell_to_fill] = None;
 }
